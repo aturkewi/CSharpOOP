256f4b8e22c05f9392bbf14dff80dd9e7d22f470
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-19 13:20:15 -0500

    Add some class inheritance

--- CSharpOOP/Item.cs
+++ CSharpOOP/Item.cs
@@ -7,5 +7,26 @@ namespace CSharpOOP
 		public int ID { get; set; }
 		public string Name { get; set; }
 	}
+
+	class Software : Item
+	{
+		public string ISBN { get; set;}
+	}
+
+	class Hardware : Item
+	{
+		public string SerialNumber { get; set;}
+	}
+
+	class Computer : Hardware
+	{
+		string CPUType { get; set;}
+		string Disks { get; set;}
+	}
+
+	class Peripheral : Hardware
+	{
+		string Description { get; set;}
+	}
 }
 
