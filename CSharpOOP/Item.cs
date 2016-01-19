9b0b4c202007e82dfb41b184172bd1ac9ac3daed
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-19 13:28:15 -0500

    Add method to Item class

--- CSharpOOP/Item.cs
+++ CSharpOOP/Item.cs
@@ -6,27 +6,37 @@ namespace CSharpOOP
 	{
 		public int ID { get; set; }
 		public string Name { get; set; }
+
+		// Virtual method - it is intended to be overidden in derived classes
+		public virtual void Purchase(){
+			Console.WriteLine("Purchasing {0}", Name);
+		}
+
 	}
 
+	// Software inherits from Item
 	class Software : Item
 	{
 		public string ISBN { get; set;}
 	}
 
+	// Hardware inherits from Item
 	class Hardware : Item
 	{
 		public string SerialNumber { get; set;}
 	}
 
+	// Computer inherits from Hardware
 	class Computer : Hardware
 	{
-		string CPUType { get; set;}
-		string Disks { get; set;}
+		public string CPUType { get; set;}
+		public string Disks { get; set;}
 	}
 
+	// Peripheral inherits from Hardware
 	class Peripheral : Hardware
 	{
-		string Description { get; set;}
+		public string Description { get; set;}
 	}
 }
 
--- CSharpOOP/Program.cs
+++ CSharpOOP/Program.cs
@@ -6,9 +6,25 @@ namespace CSharpOOP
 	{
 		public static void Main (string[] args)
 		{
+			/* 
 			var newWarehouse = new Warehouse ("Warehouse 1", 101);
 			var myItem = newWarehouse.FindAndReturnItem (101);
 			Console.WriteLine ("My item is {0}", myItem.Name);
+			*/
+
+			var myComputer = new Computer ();
+			myComputer.ID = 102;
+
+
+			Item mySecondComputer = new Computer ();
+			// Only have access to properties that and Item would have. 
+			mySecondComputer.ID = 103;
+			// 'as' treate object as if it is another class.
+			Computer mySecondComputer2 = mySecondComputer as Computer;
+			string myCPUType = mySecondComputer2.CPUType;
+			mySecondComputer2.Name = "Jupiter";
+			mySecondComputer2.Purchase ();
+
 		}
 	}
 }
